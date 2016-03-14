c726d99c8efb87b08286df38ea925cac97db0d05
hashemi rafsan <rafsanhashemi@gmail.com>
2016-03-15 03:11:21 +0600

    slack command v.1

---
+++ php/timer.php
@@ -0,0 +1,28 @@
+<?php  
+
+
+        include("db.php");
+ 
+$sql = "select remove from admin ";
+$data = mysql_query($sql);
+date_default_timezone_set("Asia/Dacca");
+while($row = mysql_fetch_array($data))
+{
+   
+    $time = $row['remove'];
+    $timer = date("Y-m-d H:i:s");
+      $timer = date("Y-m-d H:i:s");
+
+   if($time <= $timer)
+   {
+       $sqlon = "UPDATE admin SET yellow = 0 WHERE  remove = '$time'";
+       $data1 = mysql_query($sqlon);
+            
+            if ($data1)
+            {
+              
+            }
+   }
+    
+}
+?>
