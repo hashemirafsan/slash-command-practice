2cdeb14ddf151ae3fd76bf2a5a7b086fab2f02cc
hashemi rafsan <rafsanhashemi@gmail.com>
2016-03-28 15:04:02 +0600

    trying to OOP

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+		<title>404 Eror</title>
+		<meta name="keywords" content="a" />
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+	</head>
+	<body>
+		<!--start-wrap--->
+		<div class="wrap">
+			<!---start-header---->
+				<div class="header">
+					<div class="logo">
+						<h1><a href="#">Ohh</a></h1>
+					</div>
+				</div>
+				<!---728x90--->
+			<!--start-content------>
+			<div class="content">
+				<img src="images/error-img.png" title="error" />
+				<!---728x90--->
+
+				<p><span><label>O</label>hh.....</span>You Requested the page that is no longer There.</p>
+	
+				<!---728x90--->
+				
+   			</div>
+			<!--End-Cotent------>
+		</div>
+		<!--End-wrap--->
+	</body>
+
+<!-- Mirrored from p.w3layouts.com/demos/ohh/web/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 27 Mar 2016 22:17:30 GMT -->
+</html>
+
