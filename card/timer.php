064b62aca08c2c46b1f1f9478cfb7884a1093bb9
Rafsan Jani <rafsanhashemi@gmail.com>
2016-03-28 23:17:08 +0600

    Updated OOP

---
+++ card/timer.php
@@ -0,0 +1,29 @@
+<?php  
+
+
+   class timer{
+       public function time(){
+            include("db.php");
+
+    $sql ="SELECT remove FROM admin";
+    $data = $mysqli->query($sql);
+    date_default_timezone_set("Asia/Dacca");
+    while($row = $data->fetch_assoc())
+    {
+
+        $time = $row['remove'];
+        $timer = date("Y-m-d H:i:s");
+        $timer = date("Y-m-d H:i:s");
+
+        if($time <= $timer)
+        {
+            $sqlon = sprintf("UPDATE admin SET yellow = 0 WHERE  remove = '%s'",$mysqli->real_escape_string($time));
+            $data1 = $mysqli->query($sqlon);
+        }
+    }
+           
+       } 
+   } 
+    $great = new timer;
+    $great -> time();
+?>
