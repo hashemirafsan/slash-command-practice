c9fd83f1ba3fc86ef71ecaa0646b478e7496c86f
Rafsan Jani <rafsanhashemi@gmail.com>
2016-03-28 15:26:59 +0600

    Update timer.php

--- timer.php
+++ timer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php  
 
 
@@ -27,27 +26,4 @@
    } 
     $great = new timer;
     $great -> time();
-=======
-<?php  
-
-
-    include("db.php");
-
-    $sql = "select remove from admin ";
-    $data = mysql_query($sql);
-    date_default_timezone_set("Asia/Dacca");
-    while($row = mysql_fetch_array($data))
-    {
-
-        $time = $row['remove'];
-        $timer = date("Y-m-d H:i:s");
-        $timer = date("Y-m-d H:i:s");
-
-        if($time <= $timer)
-        {
-            $sqlon = "UPDATE admin SET yellow = 0 WHERE  remove = '$time'";
-            $data1 = mysql_query($sqlon);
-        }
-    }
->>>>>>> 44bc7b23d9af34ce8a2783f2637bffb12da4ecdf
 ?>
